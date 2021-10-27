9c51eae081a176475ca86478e6c4ed0d93f43e4d
Burak Velioglu <buvelio@microsoft.com>
2021-10-27 11:55:59 +0300

    Sync pg_dist_object on an update and propagate while syncing to a new node

--- src/backend/distributed/sql/downgrades/citus--11.0-1--10.2-4.sql
+++ src/backend/distributed/sql/downgrades/citus--11.0-1--10.2-4.sql
@@ -1,5 +1,9 @@
 -- citus--11.0-1--10.2-4
 
+DROP FUNCTION pg_catalog.fix_all_partition_shard_index_names();
+DROP FUNCTION pg_catalog.fix_partition_shard_index_names(regclass);
+DROP FUNCTION pg_catalog.worker_fix_partition_shard_index_names(regclass, text, text);
+
 CREATE FUNCTION pg_catalog.master_apply_delete_command(text)
     RETURNS integer
     LANGUAGE C STRICT
@@ -39,3 +43,5 @@ CREATE FUNCTION pg_catalog.citus_disable_node(nodename text, nodeport integer)
         AS 'MODULE_PATHNAME', $$citus_disable_node$$;
 COMMENT ON FUNCTION pg_catalog.citus_disable_node(nodename text, nodeport integer)
         IS 'removes node from the cluster temporarily';
+
+DROP FUNCTION pg_catalog.citus_internal_add_object_metadata(text, text[], text[], integer, integer);
