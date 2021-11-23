b6c42883ced037e74d4e5356dfec81b95001af12
Ahmet Gedemenli <afgedemenli@gmail.com>
2021-11-23 19:42:06 +0300

    Add test for propagating server ops

--- src/test/regress/sql/multi_create_fdw.sql
+++ src/test/regress/sql/multi_create_fdw.sql
@@ -16,3 +16,21 @@ set citus.enable_ddl_propagation to off;
 CREATE FOREIGN DATA WRAPPER fake_fdw HANDLER fake_fdw_handler;
 CREATE SERVER fake_fdw_server FOREIGN DATA WRAPPER fake_fdw;
 set citus.enable_ddl_propagation to on;
+
+-- test propagating foreign server creation
+CREATE EXTENSION postgres_fdw;
+CREATE SERVER foreign_server
+        FOREIGN DATA WRAPPER postgres_fdw
+        OPTIONS (host 'localhost', port :'master_port', dbname 'regression');
+
+SELECT * FROM pg_foreign_server WHERE srvname = 'foreign_server' ORDER BY srvname;
+\c - - - :worker_1_port
+-- verify that the server is created on the worker
+SELECT * FROM pg_foreign_server WHERE srvname = 'foreign_server' ORDER BY srvname;
+\c - - - :master_port
+
+DROP SERVER foreign_server;
+\c - - - :worker_1_port
+-- verify that the server is dropped on the worker
+SELECT * FROM pg_foreign_server WHERE srvname = 'foreign_server' ORDER BY srvname;
+\c - - - :master_port
